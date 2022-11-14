972b29e29f37e6a4efc69cc6315bb93aa3352a7c
Dakota Walsh <kota@nilsu.org>
2022-11-14 17:07:08 +1300

    log error in displayInvetory

--- display.go
+++ display.go
@@ -1,12 +1,16 @@
 package main
 
-import "path/filepath"
+import (
+	"log"
+	"path/filepath"
+)
 
 // displayInvetory prints out a pretty table showing owner's inventory.
 func (b backpack) displayInvetory(owner string, pricedOnly bool) string {
 	path := filepath.Join(b.dir, owner+".csv")
 	recs, err := loadRecords(path)
 	if err != nil {
+		log.Printf("error displaying inventory %v: %v", owner, err)
 		return FATAL_MSG
 	}
 	if pricedOnly {
